e93f4f7903b9dcf399342aac8faaaf923913bd03
MaramFahad <maramll98ll@gmail.com>
2023-08-13 16:56:30 +0300

    server installed

---
+++ GreenLand/GreenLand/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GreenLand project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GreenLand.settings')
+
+application = get_wsgi_application()
